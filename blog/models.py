2c3428bf06839f21d5962960bd96d6f13b2ed9a8
Ben Jones <ben.jones@myport.ac.uk>
2020-05-25 19:07:24 +0100

    Added views to create/edit blog post inside the site.

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    #created_first = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
